376456f7522a82a0471bc67f8a914f57ef21b104
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2021-09-13 10:58:53 -0300

    Second commit

--- grafico.py
+++ grafico.py
@@ -3,20 +3,24 @@ import numpy as np
 import pandas as pd
 from matplotlib import cm 
 
-zfile = pd.read_csv("./dados_Res.txt",  header=None )
+zfile = pd.read_csv("dados_Res.txt", header=None)
 z = np.array(zfile.values.flatten())
 
 x,y = np.meshgrid(np.linspace(-1,1,201), np.linspace(-1,1,201))
 
 
-fig = plt.figure(figsize=[12,8])
+fig = plt.figure(figsize=[15,12])
 ax = fig.gca(projection='3d')
 
-ax.set_xlabel('Eixo x', fontsize=15)
-ax.set_ylabel('Eixo y', fontsize=15)
-ax.set_zlabel('Eixo z', fontsize = 15)
+
+ax.set_xlabel('Eixo x', fontsize=13)
+ax.set_ylabel('Eixo y', fontsize=13)
+ax.set_zlabel('Eixo z', fontsize=13)
 
 arr_2d = np.reshape(z, (201, 201))
-ax.plot_surface(x,y,arr_2d, cmap=cm.coolwarm)
+
+surf = ax.plot_surface(x,y,arr_2d, cmap=cm.coolwarm)
+ax.set_title('Grafico de Resultado da Funcao Multimodal')
+fig.colorbar(surf, ax = ax, shrink = 0.5, aspect =5)
 
 plt.show()
